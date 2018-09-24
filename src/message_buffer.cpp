f819e6b70446b0ec2a2f9a1eabd36f004bdcb6fb
Masaya Kataoka <ms.kataoka@gmail.com>
2018-09-24 23:26:25 +0900

    add _update_events function

--- src/message_buffer.cpp
+++ src/message_buffer.cpp
@@ -17,13 +17,28 @@ message_buffer::~message_buffer()
 
 }
 
+void message_buffer::write_buffer()
+{
+    boost::mutex::scoped_lock look(_mtx);
+    return;
+}
+
 void message_buffer::_callback(ros::MessageEvent<topic_tools::ShapeShifter const> msg_event, std::string const& topic, boost::shared_ptr<ros::Subscriber> subscriber, boost::shared_ptr<int> count)
 {
     boost::mutex::scoped_lock look(_mtx);
     _message_events.push_back(msg_event);
+    _update_events();
 }
 
 void message_buffer::_update_events()
 {
     boost::mutex::scoped_lock look(_mtx);
+    std::vector<ros::MessageEvent<topic_tools::ShapeShifter const> > _new_message_events;
+    for(int i=0; i<_message_events.size(); i++)
+    {
+        if(_message_events[i].getReceiptTime() > ros::Time::now() - _buffer_length)
+        {
+            _new_message_events.push_back(_message_events[i]);
+        }
+    }
 }
